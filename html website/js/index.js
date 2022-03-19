5548e4058cec5ece2b747fe176d9a0f1dd999ea2
karanbainzy <101939911+karanbainzy@users.noreply.github.com>
2022-03-19 15:58:42 +0000

    Add files via upload

---
+++ html website/js/index.js
@@ -0,0 +1,72 @@
+    //let FirstName ='Karan'; //this is a string literial
+	//let LastName ='Bains'; //this is also a string literal
+	//let IsApproved = false; //this is a example of boolean literal it can be true or false
+	let person = {
+		FirstName:'Karan',
+		LastName: 'Bains',
+		Age:'14'  };
+
+    
+   // const SelectedColors = ['red', 'yellow' ,'blue']  //this a listing array
+
+   // document.getElementById('yo').innerHTML = function button (argument) {//
+    	// body...
+  
+console.log(
+
+//function myFunction() {
+  //document.getElementById("button").innerHTML = 'hi sir';//
+  
+}//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //this is called preforming a task function
+    //function greet (FirstName,LastName)
+    {
+    	//console.log('Hello' + FirstName + LastName)
+    }
+//greet('Karan','Bains')
+        // this is called preforming a task function 
+
+
+
+
+       // function circle(number)
+
+     //   {return number * number;}
+
+    //    let number 33 = circle(10);
+     //   console.log(number);
+
+          //this is a caculating function
+
+
+
+
